1bad4e82abd0c7b04fc8ebf71ce39a4be7a87a93
krypcide <158832710+krypcide@users.noreply.github.com>
2025-07-15 10:19:34 +0200

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,6 @@
+sudo pacman -S geany geany-plugins virtualbox flatpak coreutils grep sh wget curl git
+sudo pacman -Syu
+curl -fsS https://dl.brave.com/install.sh | sh
+sudo pacman -R firefox thunderbird
+sudo pacman -s
+sudo reboot
